59ea03232ea0b5b5ff1550305bef1389be44d1f5
GermanPy <germandrile.py@gmail.com>
2025-09-21 22:31:44 -0300

    primer archivo

---
+++ Golf1.py
@@ -0,0 +1,5 @@
+#Proyecto de prueba de Git Github
+s = input ("Ingresa tu nombre: ")
+print ("Sistema de control de versiones")
+print ("-----Este es el punto de inicio-----")
+print ("Bienvenido ",s)
