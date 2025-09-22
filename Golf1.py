57482882157754b1fb33b96c0f28c1c920946fab
Germán <germandrile.py@gmail.com>
2025-09-22 15:01:29 -0300

    Update Golf1.py
    sumado codigo de seguridad

--- Golf1.py
+++ Golf1.py
@@ -3,3 +3,7 @@ s = input ("Ingresa tu nombre: ")
 print ("Sistema de control de versiones")
 print ("-----Este es el punto de inicio-----")
 print ("Bienvenido ",s)
+print ("Ahora ingrese numero clave")
+k = imput ("Numero es") 
+t = k *25 + 100
+print ("Su numero de seguridad es: ". t) 
